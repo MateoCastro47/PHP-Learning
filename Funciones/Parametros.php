a8dd53135c191b7b429641c46328f3763f81c42f
MateoCastro47 <castrosoutomateo@gmail.com>
2026-06-11 11:04:18 +0200

    Arreglado error

--- Funciones/Parametros.php
+++ Funciones/Parametros.php
@@ -6,8 +6,10 @@
         }
         echo "El factorial de $numero es: " . $resultado;
 
-        factorial(5); // Haces factorial de 5;
-        factorial(9); // Haces factorial de 9, reutilizando la función;
+        
     }
 
+    factorial(5); // Haces factorial de 5;
+    echo "<br>";
+    factorial(9); // Haces factorial de 9, reutilizando la función;
 ?>
